90fad2143436ded39c4fbc5926ec4f3d5fc41eb4
Solvita <solvita.dvortsova@henleyglobal.com>
2022-10-20 17:08:10 +0300

    content

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,323 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+    <v-content>
+      <div id="app" data-app>
+        <vue-html2pdf
+          :show-layout="false"
+          :float-layout="true"
+          :enable-download="false"
+          :preview-modal="true"
+          :paginate-elements-by-height="1400"
+          filename="print"
+          :pdf-quality="2"
+          :manual-pagination="false"
+          pdf-format="a4"
+          pdf-orientation="portrait"
+          pdf-content-width="800px"
+          @progress="onProgress($event)"
+          @hasStartedGeneration="hasStartedGeneration()"
+          @hasGenerated="hasGenerated($event)"
+          ref="html2Pdf"
+        >
+          <pdf-content
+            @domRendered="domRendered()"
+            slot="pdf-content"
+            :form="form"
+          />
+        </vue-html2pdf>
+        <v-card class="mx-auto" max-width="800">
+          <v-card-text>
+            <v-form ref="form" v-model="valid" lazy-validation>
+              <v-text-field
+                v-model="form.firstname"
+                :rules="requiredRules"
+                label="First name(s)"
+                required
+              ></v-text-field>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+              <v-text-field
+                v-model="form.lastname"
+                :rules="requiredRules"
+                label="Last name"
+                required
+              ></v-text-field>
+
+              <v-text-field
+                v-model="form.othername"
+                label="Other name(s) you are known by"
+              ></v-text-field>
+
+              <v-text-field
+                v-model="form.nationality"
+                label="Nationality"
+                :rules="requiredRules"
+                required
+              ></v-text-field>
+
+              <v-text-field
+                v-model="form.placeOfBirth"
+                label="Place of birth"
+                :rules="requiredRules"
+                required
+              ></v-text-field>
+
+              <v-menu
+                ref="menu"
+                v-model="menu"
+                :close-on-content-click="false"
+                transition="scale-transition"
+                offset-y
+                min-width="auto"
+              >
+                <template v-slot:activator="{ on, attrs }">
+                  <v-text-field
+                    v-model="form.date"
+                    label="Date of birth"
+                    readonly
+                    v-bind="attrs"
+                    v-on="on"
+                    :rules="requiredRules"
+                  ></v-text-field>
+                </template>
+                <v-date-picker
+                  v-model="form.date"
+                  :active-picker.sync="activePicker"
+                  max="2000-01-01"
+                  min="1950-01-01"
+                  @change="save"
+                ></v-date-picker>
+              </v-menu>
+
+              <v-text-field
+                v-model="form.address"
+                label="Permanent residential address:"
+                :rules="requiredRules"
+                required
+              ></v-text-field>
+
+              <v-text-field
+                v-model="form.email"
+                :rules="emailRules"
+                label="E-mail"
+                required
+              ></v-text-field>
+
+              <v-text-field
+                v-model="form.occupation"
+                label="Occupation"
+                :rules="requiredRules"
+                required
+              ></v-text-field>
+
+              <v-text-field
+                v-model="form.activities"
+                label="Business activities"
+                :rules="requiredRules"
+                required
+              ></v-text-field>
+
+              <v-text-field
+                v-model="form.nameAndAddress"
+                label="Business name and address"
+                :rules="requiredRules"
+                required
+              ></v-text-field>
+
+              <v-text-field
+                v-model="form.purpose"
+                label="Purpose of seeking alternative citizenship / residence"
+                :rules="requiredRules"
+                required
+              ></v-text-field>
+
+              <v-text-field
+                v-model="form.program"
+                label="Program of interest"
+                :rules="requiredRules"
+                required
+              ></v-text-field>
+
+              <v-radio-group
+                v-model="form.pep"
+                label="Are you a Politically Exposed Person (“PEP”)?"
+                :rules="requiredRulesRadio"
+                required
+                row
+              >
+                <v-radio label="No" :value="false"></v-radio>
+                <v-radio label="Yes" :value="true"></v-radio>
+              </v-radio-group>
+
+              <v-radio-group
+                v-model="form.association"
+                label="Do you have any association with a previous applicant or an individual with an ongoing application for the program of interest (i.e. family members, associates, business partners, etc.)?"
+                :rules="requiredRulesRadio"
+                required
+                row
+              >
+                <v-radio label="No" :value="false"></v-radio>
+                <v-radio label="Yes" :value="true"></v-radio>
+              </v-radio-group>
+
+              <v-textarea
+                v-if="form.association"
+                v-model="form.associationDesc"
+                label="Please enter name of individual(s) and describe relationship"
+                :rules="requiredRules"
+                required
+              ></v-textarea>
+
+              <v-radio-group
+                v-model="form.rejected"
+                label="Have you or has one of your dependents ever been rejected by a government authority for a visa, residence or citizenship application?"
+                :rules="requiredRulesRadio"
+                required
+                row
+              >
+                <v-radio label="No" :value="false"></v-radio>
+                <v-radio label="Yes" :value="true"></v-radio>
+              </v-radio-group>
+
+              <v-textarea
+                v-if="form.rejected"
+                v-model="form.rejectedDesc"
+                label="Please enter name of country and type of application"
+                :rules="requiredRules"
+                required
+              ></v-textarea>
+            </v-form>
+          </v-card-text>
+
+          <v-card-actions
+            ><v-btn color="primary" @click="downloadPdf()"
+              >Print PDF</v-btn
+            ></v-card-actions
+          >
+        </v-card>
       </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld/>
-    </v-main>
+    </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import PdfContent from "@/components/PdfContent";
+import VueHtml2pdf from "vue-html2pdf";
 
 export default {
-  name: 'App',
+  name: "app",
 
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      valid: false,
+      form: {
+        firstname: "",
+        lastname: "",
+        othername: "",
+        nationality: "",
+        placeOfBirth: "",
+        date: null,
+        address: "",
+        occupation: "",
+        activities: "",
+        nameAndAddress: "",
+        purpose: "",
+        program: "",
+        pep: "",
+        association: "",
+        associationDesc: "",
+        rejected: "",
+        rejectedDesc: "",
+        email: "",
+      },
+
+      activePicker: null,
+      menu: false,
+      requiredRules: [(v) => !!v || "This field is required"],
+      requiredRulesRadio: [
+        (v) => typeof v === "boolean" || "This field is required",
+      ],
+
+      emailRules: [
+        (v) => !!v || "E-mail is required",
+        (v) => /.+@.+/.test(v) || "E-mail must be valid",
+      ],
+      contentRendered: false,
+      progress: 0,
+      generatingPdf: false,
+      pdfDownloaded: false,
+    };
+  },
+  watch: {
+    menu(val) {
+      val && setTimeout(() => (this.activePicker = "YEAR"));
+    },
+  },
+  methods: {
+    save(date) {
+      this.$refs.menu.save(date);
+    },
+    downloadPdf() {
+      this.$refs.form.validate();
+      const valid = this.$refs.form.validate();
+      if (valid) {
+        this.$refs.html2Pdf.generatePdf();
+      }
+    },
+
+    onProgress(progress) {
+      this.progress = progress;
+      console.log(`PDF generation progress: ${progress}%`);
+    },
+
+    startPagination() {
+      console.log(`PDF has started pagination`);
+    },
+
+    hasPaginated() {
+      console.log(`PDF has been paginated`);
+    },
+
+    // async beforeDownload({ html2pdf, options, pdfContent }) {
+    //   console.log(`On Before PDF Generation`);
+    //   // await html2pdf().set(options).from(pdfContent).toPdf().get('pdf').then((pdf) => {
+    //   // 	const totalPages = pdf.internal.getNumberOfPages()
+    //   // 	for (let i = 1; i <= totalPages; i++) {
+    //   // 		pdf.setPage(i)
+    //   // 		pdf.setFontSize(10)
+    //   // 		pdf.setTextColor(150)
+    //   // 		pdf.text('Page ' + i + ' of ' + totalPages, (pdf.internal.pageSize.getWidth() * 0.88), (pdf.internal.pageSize.getHeight() - 0.3))
+    //   // 	}
+    //   // }).save()
+    // },
+
+    hasDownloaded(blobPdf) {
+      console.log(`PDF has downloaded yehey`);
+      this.pdfDownloaded = true;
+      console.log(blobPdf);
+    },
+
+    domRendered() {
+      console.log("Dom Has Rendered");
+      this.contentRendered = true;
+    },
+
+    onBlobGenerate(blob) {
+      console.log(blob);
+    },
   },
 
-  data: () => ({
-    //
-  }),
+  components: {
+    VueHtml2pdf,
+    PdfContent,
+  },
 };
 </script>
+
+<style lang="scss">
+html,
+body {
+  width: 100%;
+  padding: 0;
+  margin: 0;
+}
+</style>
---
+++ src/components/PdfContent.vue
@@ -0,0 +1,245 @@
+<template>
+  <section slot="pdf-content" class="pdf-content">
+    <v-row style=" margin-bottom: 0px, padding-bottom: 0px">
+      <v-col>
+        <div style="border-bottom: 1px solid #75767a; margin: 0px">
+          <v-img
+            style="margin-bottom: 17px"
+            contain
+            height="35px"
+            src="../assets/logo.png"
+            lazy-src="../assets/logo.png"
+          ></v-img>
+        </div>
+      </v-col>
+    </v-row>
+    <v-row style="margin-top: 0px, padding-top: 0px; ">
+      <v-col style="margin-top: -20px, padding-top: 0px; ">
+        <h1 style="margin-top: -5px">DUE DILIGENCE FORM A</h1>
+        <h1>1. CLIENT (CONTRACTING PARTY)</h1>
+        <p>
+          In order for us to conduct proper due diligence on you, we require
+          certain personal information from you. You are kindly requested to
+          enclose a copy of your passport with this Form A.
+        </p>
+
+        <p>Surname:</p>
+        <div class="field">
+          <span>
+            {{ form.lastname }}
+          </span>
+        </div>
+        <p>First name(s):</p>
+        <div class="field">
+          <span>
+            {{ form.firstname }}
+          </span>
+        </div>
+        <p>Other name(s) you are known by:</p>
+        <div class="field">
+          <span>
+            {{ form.othername ? form.othername : " N/A" }}
+          </span>
+        </div>
+        <p>Nationality:</p>
+        <div class="field">
+          <span>
+            {{ form.nationality }}
+          </span>
+        </div>
+        <p>Place of birth/date of birth:</p>
+        <div class="field">
+          <span>
+            {{ form.placeOfBirth + " " + form.date }}
+          </span>
+        </div>
+        <p>Permanent residential address:</p>
+        <div class="field">
+          <span>
+            {{ form.address }}
+          </span>
+        </div>
+        <p>Email address:</p>
+        <div class="field">
+          <span>
+            {{ form.email }}
+          </span>
+        </div>
+        <p>Occupation:</p>
+        <div class="field">
+          <span>
+            {{ form.occupation }}
+          </span>
+        </div>
+        <p>Business activities:</p>
+        <div class="field">
+          <span>
+            {{ form.activities }}
+          </span>
+        </div>
+        <p>Business name and address:</p>
+        <div class="field">
+          <span>
+            {{ form.nameAndAddress }}
+          </span>
+        </div>
+        <p>Purpose of seeking alternative citizenship / residence:</p>
+        <div class="field">
+          <span>
+            {{ form.purpose }}
+          </span>
+        </div>
+      </v-col>
+      <v-col style="margin-top: -20px, padding-top: 0px; ">
+        <p style="margin-top: -5px">Program of interest:</p>
+        <div class="field">
+          <span>
+            {{ form.program }}
+          </span>
+        </div>
+        <p>Are you a Politically Exposed Person (“PEP”)?</p>
+        <div class="boolfield">
+          <span v-if="form.pep">
+            <v-icon> mdi-checkbox-blank-outline</v-icon>No
+            <v-icon> mdi-checkbox-marked </v-icon> Yes
+          </span>
+          <span v-else>
+            <v-icon> mdi-checkbox-marked </v-icon> No
+            <v-icon> mdi-checkbox-blank-outline</v-icon> Yes
+          </span>
+        </div>
+        <p>
+          While there is no global definition of a PEP, the Financial Action
+          Task Force has issued guidelines (www.fatf-gafi.org), which generally
+          refer to the following: A PEP is a current or former (during the past
+          ten years) senior official in the executive, legislative,
+          administrative, military, or judicial branch of the state (elected or
+          not), a senior official of a major political party, a senior executive
+          of a government owned commercial enterprise, and/or being a
+          corporation, business or other entity formed by or for the benefit of
+          any such individual; an immediate family member of such individual,
+          meaning spouse, parents, siblings, children, and spouse’s parents or
+          siblings, or any individual publicly known (or actually known by the
+          relevant financial institution) to be a close personal or professional
+          associate. If you are in doubt whether you qualify as PEP or not,
+          please tick Yes above.
+        </p>
+
+        <p>
+          Do you have any association with a previous applicant or an individual
+          with an ongoing application for the program of interest (i.e. family
+          members, associates, business partners, etc.)?
+        </p>
+        <div class="boolfield">
+          <span v-if="form.association">
+            <v-icon> mdi-checkbox-blank-outline</v-icon>No
+            <v-icon> mdi-checkbox-marked </v-icon> Yes
+          </span>
+          <span v-else>
+            <v-icon> mdi-checkbox-marked </v-icon> No
+            <v-icon> mdi-checkbox-blank-outline</v-icon> Yes
+          </span>
+        </div>
+        <p>
+          If you ticked yes, please enter name of individual(s) and describe
+          relationship:
+        </p>
+        <div class="field">
+          <span>
+            {{ form.associationDesc ? form.associationDesc : " N/A" }}
+          </span>
+        </div>
+        <p>
+          Have you or has one of your dependents ever been rejected by a
+          government authority for a visa, residence or citizenship application?
+        </p>
+        <div class="boolfield">
+          <span v-if="form.rejected">
+            <v-icon> mdi-checkbox-blank-outline</v-icon>No
+            <v-icon> mdi-checkbox-marked </v-icon> Yes
+          </span>
+          <span v-else>
+            <v-icon> mdi-checkbox-marked </v-icon> No
+            <v-icon> mdi-checkbox-blank-outline</v-icon> Yes
+          </span>
+        </div>
+        <p>
+          If you ticked yes, please enter name of country and type of
+          application:
+        </p>
+        <div class="field">
+          <span>
+            {{ form.rejectedDesc ? form.rejectedDesc : " N/A" }}
+          </span>
+        </div>
+      </v-col>
+    </v-row>
+    <v-row style=" margin-bottom: 0px, padding-bottom: 0px">
+      <v-col>
+        <div class="bottom">
+          <p>
+            HP Due Diligence Form A V211017
+
+            <br />
+            Copyright © 2021 by Henley &amp; Partners Group Holdings Ltd. All
+            rights reserved.
+          </p>
+        </div>
+      </v-col>
+    </v-row>
+  </section>
+</template>
+<script>
+export default {
+  props: {
+    form: Object,
+  },
+  data() {
+    return {};
+  },
+
+  mounted() {
+    this.$nextTick(() => {
+      setTimeout(() => {
+        this.$emit("domRendered");
+      }, 1000);
+    });
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.pdf-content {
+  width: 100%;
+
+  background: #fff;
+  padding: 17px 47px 17px 47px;
+  font-family: "Times New Roman", Times, serif;
+  font-weight: 200;
+  line-height: 1.3;
+}
+p,
+h1 {
+  margin-top: 20px;
+  margin-bottom: 20px;
+}
+
+div .field {
+  border: 1px solid #75767a;
+  padding: 2px;
+  margin-top: -15px;
+  margin-bottom: -15px;
+}
+
+div .boolfield {
+  margin-top: -15px;
+  margin-bottom: -15px;
+}
+
+.bottom p {
+  line-height: 1;
+  font-size: small;
+
+  margin-bottom: 0px;
+}
+</style>
